5f79f19fdbc130de10f04608404e2b0d38396a43
orferr <154352729+orferr@users.noreply.github.com>
2023-12-19 17:14:40 +0100

    Create rec6udJCjMHfik1en.java

---
+++ rec6udJCjMHfik1en.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] array = {5, 2, 9, 1, 5, 6};
+        
+        System.out.println("Before sorting: " + Arrays.toString(array));
+        
+        quickSort(array, 0, array.length - 1);
+        
+        System.out.println("After sorting: " + Arrays.toString(array));
+    }
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int partitionIndex = partition(array, low, high);
+
+            quickSort(array, low, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+
+        swap(array, i + 1, high);
+
+        return i + 1;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
